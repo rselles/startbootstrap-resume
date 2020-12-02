0b709b6f858313a026ddeb112a194316ba66b8d0
Rafael Sellés <rafaelselles@gmail.com>
2020-12-02 01:04:34 +0100

    Fix path for favicons

--- gulpfile.js
+++ gulpfile.js
@@ -10,73 +10,73 @@ var browserSync = require('browser-sync').create();
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+    ' */\n',
+    '\n'
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function () {
 
-  // Bootstrap
-  gulp.src([
-    './node_modules/bootstrap/dist/**/*',
-    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-  ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    // Bootstrap
+    gulp.src([
+        './node_modules/bootstrap/dist/**/*',
+        '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+        '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+        .pipe(gulp.dest('./vendor/bootstrap'))
 
-  // Font Awesome
-  gulp.src([
-    './node_modules/@fortawesome/**/*',
-  ])
-    .pipe(gulp.dest('./vendor'))
+    // Font Awesome
+    gulp.src([
+        './node_modules/@fortawesome/**/*',
+    ])
+        .pipe(gulp.dest('./vendor'))
 
-  // jQuery
-  gulp.src([
-    './node_modules/jquery/dist/*',
-    '!./node_modules/jquery/dist/core.js'
-  ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    // jQuery
+    gulp.src([
+        './node_modules/jquery/dist/*',
+        '!./node_modules/jquery/dist/core.js'
+    ])
+        .pipe(gulp.dest('./vendor/jquery'))
 
-  // jQuery Easing
-  gulp.src([
-    './node_modules/jquery.easing/*.js'
-  ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+    // jQuery Easing
+    gulp.src([
+        './node_modules/jquery.easing/*.js'
+    ])
+        .pipe(gulp.dest('./vendor/jquery-easing'))
 
 });
 
 // Compile SCSS
 gulp.task('css:compile', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./css'))
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass.sync({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('./css'))
 });
 
 // Minify CSS
 gulp.task('css:minify', ['css:compile'], function () {
-  return gulp.src([
-    './css/*.css',
-    '!./css/*.min.css'
-  ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+    return gulp.src([
+        './css/*.css',
+        '!./css/*.min.css'
+    ])
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 });
 
 // CSS
@@ -84,19 +84,19 @@ gulp.task('css', ['css:compile', 'css:minify']);
 
 // Minify JavaScript
 gulp.task('js:minify', function () {
-  return gulp.src([
-    './js/*.js',
-    '!./js/*.min.js'
-  ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.stream());
+    return gulp.src([
+        './js/*.js',
+        '!./js/*.min.js'
+    ])
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('./js'))
+        .pipe(browserSync.stream());
 });
 
 // JS
@@ -107,18 +107,18 @@ gulp.task('default', ['css', 'js', 'vendor']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 // Dev task
 gulp.task('dev', ['css', 'js', 'browserSync'], function () {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
+    gulp.watch('./scss/*.scss', ['css']);
+    gulp.watch('./js/*.js', ['js']);
+    gulp.watch('./*.html', browserSync.reload);
 });
 
 var realFavicon = require('gulp-real-favicon');
@@ -132,80 +132,80 @@ var FAVICON_DATA_FILE = 'faviconData.json';
 // you should run it whenever RealFaviconGenerator updates its
 // package (see the check-for-favicon-update task below).
 gulp.task('generate-favicon', function (done) {
-  realFavicon.generateFavicon({
-    masterPicture: 'img/profile.jpg',
-    dest: 'img/icons',
-    iconsPath: '/',
-    design: {
-      ios: {
-        pictureAspect: 'noChange',
-        assets: {
-          ios6AndPriorIcons: false,
-          ios7AndLaterIcons: false,
-          precomposedIcons: false,
-          declareOnlyDefaultIcon: true
+    realFavicon.generateFavicon({
+        masterPicture: 'img/profile.jpg',
+        dest: 'img/icons',
+        iconsPath: 'img/icons',
+        design: {
+            ios: {
+                pictureAspect: 'noChange',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                },
+                appName: 'Rafael Sellés - Backend Developer - Resume / CV'
+            },
+            desktopBrowser: {
+                design: 'raw'
+            },
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#ff0000',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: true,
+                        rectangle: false
+                    }
+                },
+                appName: 'Rafael Sellés - Backend Developer - Resume / CV'
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    name: 'Rafael Sellés - Backend Developer - Resume / CV',
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'blackAndWhite',
+                threshold: 50,
+                themeColor: '#5bbad5'
+            }
         },
-        appName: 'Rafael Sellés - Backend Developer - Resume / CV'
-      },
-      desktopBrowser: {
-        design: 'raw'
-      },
-      windows: {
-        pictureAspect: 'noChange',
-        backgroundColor: '#ff0000',
-        onConflict: 'override',
-        assets: {
-          windows80Ie10Tile: false,
-          windows10Ie11EdgeTiles: {
-            small: false,
-            medium: true,
-            big: true,
-            rectangle: false
-          }
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
         },
-        appName: 'Rafael Sellés - Backend Developer - Resume / CV'
-      },
-      androidChrome: {
-        pictureAspect: 'noChange',
-        themeColor: '#ffffff',
-        manifest: {
-          name: 'Rafael Sellés - Backend Developer - Resume / CV',
-          display: 'standalone',
-          orientation: 'notSet',
-          onConflict: 'override',
-          declared: true
-        },
-        assets: {
-          legacyIcon: false,
-          lowResolutionIcons: false
-        }
-      },
-      safariPinnedTab: {
-        pictureAspect: 'blackAndWhite',
-        threshold: 50,
-        themeColor: '#5bbad5'
-      }
-    },
-    settings: {
-      scalingAlgorithm: 'Mitchell',
-      errorOnImageTooSmall: false,
-      readmeFile: false,
-      htmlCodeFile: false,
-      usePathAsIs: false
-    },
-    markupFile: FAVICON_DATA_FILE
-  }, function () {
-    done();
-  });
+        markupFile: FAVICON_DATA_FILE
+    }, function () {
+        done();
+    });
 });
 
 // Inject the favicon markups in your HTML pages. You should run
 // this task whenever you modify a page. You can keep this task
 // as is or refactor your existing HTML pipeline.
 gulp.task('inject-favicon-markups', function () {
-  return gulp.src(['*.html'])
-    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
-    .pipe(gulp.dest(''));
+    return gulp.src(['*.html'])
+        .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+        .pipe(gulp.dest(''));
 });
 
 // Check for updates on RealFaviconGenerator (think: Apple has just
@@ -213,10 +213,10 @@ gulp.task('inject-favicon-markups', function () {
 // Run this task from time to time. Ideally, make it part of your
 // continuous integration system.
 gulp.task('check-for-favicon-update', function (done) {
-  var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
-  realFavicon.checkForUpdates(currentVersion, function (err) {
-    if (err) {
-      throw err;
-    }
-  });
+    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    realFavicon.checkForUpdates(currentVersion, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
 });
